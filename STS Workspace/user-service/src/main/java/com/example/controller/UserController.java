6b39fae2ee0b017187f06aefdaa16a78a1065350
Debasish <sahudebasish012@gmail.com>
2026-03-29 23:57:06 +0530

    get user by id

--- STS Workspace/user-service/src/main/java/com/example/controller/UserController.java
+++ STS Workspace/user-service/src/main/java/com/example/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class UserController {
 	@GetMapping
 	public String test(){
 		return "User service is working";
+	}
+	
+	@GetMapping
+	public String getById(@PathVariable int id) { 
+		return "Get mapping is working";
 	}
 	
 	
